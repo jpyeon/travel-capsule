0ca163b8acf40e5c876e83c012c89960409ac31d
jpyeon <92893995+jpyeon@users.noreply.github.com>
2026-04-01 17:31:53 +0000

    feat: wrap app with next-themes ThemeProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
 import { Toaster } from 'sonner';
 import { AuthProvider } from '../contexts/AuthContext';
 import { ErrorBoundary } from '../components/shared/ErrorBoundary';
@@ -7,14 +8,16 @@ import { NavBar } from '../components/shared/NavBar';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Toaster position="bottom-right" richColors />
-      <div className="min-h-screen bg-sand-50">
-        <NavBar />
-        <ErrorBoundary>
-          <Component {...pageProps} />
-        </ErrorBoundary>
-      </div>
-    </AuthProvider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem storageKey="travel-capsule-theme">
+      <AuthProvider>
+        <Toaster position="bottom-right" richColors />
+        <div className="min-h-screen bg-sand-50 dark:bg-night-300">
+          <NavBar />
+          <ErrorBoundary>
+            <Component {...pageProps} />
+          </ErrorBoundary>
+        </div>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
